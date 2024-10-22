557f3c86cf206aa2f12d2dada62e8e745529bcc6
Orian <buitantrung01@gmail.com>
2024-10-22 23:32:43 +0700

    a

---
+++ src/image/index.js
@@ -0,0 +1,11 @@
+const Images = {
+    tomato: require('./tomato.png'),
+    cua: require('./cua.jpg'),
+    pe1: require('./pe1.jpg'),
+    pe2: require('./pe2.jpg'),
+    pe3: require('./pe3.jpg')
+
+
+}
+
+export default Images
